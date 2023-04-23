82476f7061123497cf0d37b0c01b3a5f30961263
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2023-04-23 15:58:56 +0300

    Add graphql schema

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/IvanBorislavovDimitrov/smart-charger/graph"
+)
+
+const defaultPort = "8081"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
+	http.Handle("/graphql", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL server", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
